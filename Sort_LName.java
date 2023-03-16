05b0871d36700a123e57d07465a9b704f1de301b
Saswat Pattnaik <48350372+The-Devil-Saswat@users.noreply.github.com>
2023-03-17 00:09:36 +0530

    Add files via upload

---
+++ Sort_LName.java
@@ -0,0 +1,40 @@
+package File_Handling;
+
+import java.io.*;
+import java.util.*;
+
+public class Sort_LName {
+
+public static void main(String[] args) throws Exception {
+		
+		BufferedReader fr=new BufferedReader(new FileReader("C:\\Users\\asus\\Desktop\\testout1.txt"));
+		FileWriter fw=new FileWriter("C:\\Users\\asus\\Desktop\\testout3.txt");
+		
+		String line=fr.readLine();
+		ArrayList<String> l = new ArrayList<String>();
+		while (line!=null)
+		{
+			int end=0;
+			
+			for (int i=0;i<line.length();i++)
+			{
+				if (line.charAt(i)==' ')
+				{
+					end=i;
+					break;
+				}
+			}
+			
+			String s=line.substring(end+1)+line.substring(0,end+1);
+			l.add(s);
+			line=fr.readLine();
+		}
+		Collections.sort(l);
+		for(String a:l)
+			fw.write(a+"\n");
+		fw.close();
+		
+
+	}
+
+}

18d8047d7afb3cffce4831cd792d16f626924555
Saswat Pattnaik <48350372+The-Devil-Saswat@users.noreply.github.com>
2023-03-16 22:57:35 +0530

    Add files via upload

---
+++ Editing_CSF_File.java
@@ -0,0 +1,36 @@
+package File_Handling;
+
+import java.io.*;
+
+public class Editing_CSF_File {
+
+public static void main(String[] args) throws Exception {
+		
+		BufferedReader fr=new BufferedReader(new FileReader("C:\\Users\\asus\\Desktop\\Add.csv"));
+		FileWriter fw=new FileWriter("C:\\Users\\asus\\Desktop\\testout.txt");
+		
+		String line=fr.readLine();
+		while (line !=null)
+		{
+			int c=0;
+			if (line.charAt(0)!=',')
+			{
+				int x=0;
+				for(int i=0;i<line.length();i++)
+				{
+					if (line.charAt(i)==',')
+						c++;
+					if(c==2)
+						x=i;
+				}
+				String s=line.substring(0, x+1);
+				fw.write(s+"\n");
+			}
+			line=fr.readLine();
+		}
+		fw.close();
+		
+
+	}
+
+}
